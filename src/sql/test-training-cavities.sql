05ca6325b78fcca0241f0b9212f941b22b272a78
Alexander Korsunsky <a.korsunsky@gmail.com>
2016-04-11 12:34:42 +0200

    Update testing cavities SQL selector

--- src/sql/test-training-cavities.sql
+++ src/sql/test-training-cavities.sql
@@ -1,7 +1,9 @@
 SELECT uuid FROM fridge_cavities
-WHERE shaped = 1
+WHERE status = 3
+AND A_n != 0 AND C_n != 0 AND d_n != 0 AND e_n !=0
+AND shaped = 1
 AND cavitysearch_uuid IN ("568BC29C-9B76-4933-8448-6190A4E9E20D", "7B2361DA-D125-4E2E-877F-450804C5CFC6" )
 AND ligands IN ("NA", "ACT", "MN", "CA", "PO4", "NAG", "HEM", "CL", "ZN", "EDO", "MG", "GOL", "SO4")
-AND volume < 1400
-AND status = 3
-AND A_n != 0 AND C_n != 0 AND d_n != 0 AND e_n !=0
+AND volume < 745 -- (0.375 * 30)^3 * 1/6 * pi
+AND volume > 20
+AND coverage_ligand_cavity > 50
