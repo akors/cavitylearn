923d10abebc6797551daf8f780edef93c286b6c4
Alexander Korsunsky <a.korsunsky@gmail.com>
2016-05-10 15:33:50 +0200

    catalonet0: Add more convolution layers, increase filters

--- src/py/cavitylearn/catalonet0.py
+++ src/py/cavitylearn/catalonet0.py
@@ -2,7 +2,6 @@
 import tensorflow as tf
 import numpy as np
 
-NUM_FILTERS = 12
 FC_SIZE = 1024
 DTYPE = tf.float32
 
@@ -18,28 +17,67 @@ def _bias_variable(name, shape):
 def inference(boxes, dataconfig):
     prev_layer = boxes
 
+    in_filters = dataconfig.num_props
     with tf.variable_scope('conv1') as scope:
-        kernel = _weight_variable('weights', [5, 5, 5, dataconfig.num_props, NUM_FILTERS])
+        out_filters = 16
+        kernel = _weight_variable('weights', [5, 5, 5, in_filters, out_filters])
         conv = tf.nn.conv3d(prev_layer, kernel, [1, 1, 1, 1, 1], padding='SAME')
-        biases = _bias_variable('biases', [NUM_FILTERS])
+        biases = _bias_variable('biases', [out_filters])
         bias = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(bias, name=scope.name)
 
-    pool1 = tf.nn.max_pool3d(conv1, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
+        prev_layer = conv1
+        in_filters = out_filters
+
+    pool1 = tf.nn.max_pool3d(prev_layer, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
     norm1 = pool1  # tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta = 0.75, name='norm1')
 
     prev_layer = norm1
 
     with tf.variable_scope('conv2') as scope:
-        kernel = _weight_variable('weights', [5, 5, 5, NUM_FILTERS, NUM_FILTERS])
+        out_filters = 32
+        kernel = _weight_variable('weights', [5, 5, 5, in_filters, out_filters])
         conv = tf.nn.conv3d(prev_layer, kernel, [1, 1, 1, 1, 1], padding='SAME')
-        biases = _bias_variable('biases', [NUM_FILTERS])
+        biases = _bias_variable('biases', [out_filters])
         bias = tf.nn.bias_add(conv, biases)
         conv2 = tf.nn.relu(bias, name=scope.name)
 
-    norm2 = conv2
-    pool2 = tf.nn.max_pool3d(norm2, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
-    prev_layer = pool2
+        prev_layer = conv2
+        in_filters = out_filters
+
+    # normalize prev_layer here
+    prev_layer = tf.nn.max_pool3d(prev_layer, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('conv3_1') as scope:
+        out_filters = 64
+        kernel = _weight_variable('weights', [5, 5, 5, in_filters, out_filters])
+        conv = tf.nn.conv3d(prev_layer, kernel, [1, 1, 1, 1, 1], padding='SAME')
+        biases = _bias_variable('biases', [out_filters])
+        bias = tf.nn.bias_add(conv, biases)
+        prev_layer = tf.nn.relu(bias, name=scope.name)
+        in_filters = out_filters
+
+    with tf.variable_scope('conv3_2') as scope:
+        out_filters = 64
+        kernel = _weight_variable('weights', [5, 5, 5, in_filters, out_filters])
+        conv = tf.nn.conv3d(prev_layer, kernel, [1, 1, 1, 1, 1], padding='SAME')
+        biases = _bias_variable('biases', [out_filters])
+        bias = tf.nn.bias_add(conv, biases)
+        prev_layer = tf.nn.relu(bias, name=scope.name)
+        in_filters = out_filters
+
+    with tf.variable_scope('conv3_3') as scope:
+        out_filters = 32
+        kernel = _weight_variable('weights', [5, 5, 5, in_filters, out_filters])
+        conv = tf.nn.conv3d(prev_layer, kernel, [1, 1, 1, 1, 1], padding='SAME')
+        biases = _bias_variable('biases', [out_filters])
+        bias = tf.nn.bias_add(conv, biases)
+        prev_layer = tf.nn.relu(bias, name=scope.name)
+        in_filters = out_filters
+
+    # normalize prev_layer here
+    prev_layer = tf.nn.max_pool3d(prev_layer, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
+
 
     with tf.variable_scope('local3') as scope:
         dim = np.prod(prev_layer.get_shape().as_list()[1:])
@@ -66,3 +104,10 @@ def inference(boxes, dataconfig):
         softmax_linear = tf.add(tf.matmul(prev_layer, weights), biases, name=scope.name)
 
     return softmax_linear
+
+
+def loss(logits, labels):
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+        logits, labels, name='cross_entropy_per_example')
+
+    return tf.reduce_mean(cross_entropy, name='xentropy_mean')
