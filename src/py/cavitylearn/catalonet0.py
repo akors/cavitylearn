fc26e0c7fefe4157ee0839550c00123ccf849bff
Alexander Korsunsky <a.korsunsky@gmail.com>
2016-06-10 16:59:56 +0200

    catalonet0: Implement l2 regularization of weights

--- src/py/cavitylearn/catalonet0.py
+++ src/py/cavitylearn/catalonet0.py
@@ -2,7 +2,6 @@
 import tensorflow as tf
 import numpy as np
 
-FC_SIZE = 1024
 DTYPE = tf.float32
 
 
@@ -19,17 +18,17 @@ def variable_summaries(var, name):
         tf.histogram_summary(name, var)
 
 
-def _weight_variable(name, shape):
-    return tf.get_variable(name, shape, DTYPE, tf.truncated_normal_initializer(stddev=0.1))
+def _weight_variable(name, shape, dtype=DTYPE):
+    return tf.get_variable(name, shape, dtype, tf.truncated_normal_initializer(stddev=0.1))
 
 
-def _bias_variable(name, shape):
-    return tf.get_variable(name, shape, DTYPE, tf.constant_initializer(0.1, dtype=DTYPE))
+def _bias_variable(name, shape, dtype=DTYPE):
+    return tf.get_variable(name, shape, dtype, tf.constant_initializer(0.1, dtype=DTYPE))
 
 
-def _convlayer(input, num_filters, layer_name, keep_prob=None, pooling=True):
+def _convlayer(input, num_filters, layer_name, keep_prob=None, pooling=True, l2scale=0.0):
 
-    with tf.variable_scope(layer_name) as scope:
+    with tf.variable_scope(layer_name, regularizer=tf.contrib.layers.l2_regularizer(l2scale)) as scope:
         kernel = _weight_variable('weights', [5, 5, 5, input.get_shape().as_list()[4], num_filters])
 
         variable_summaries(kernel, layer_name + '/weights')
@@ -56,11 +55,11 @@ def _convlayer(input, num_filters, layer_name, keep_prob=None, pooling=True):
     return output
 
 
-def _fc_layer(input, fc_size, layer_name, keep_prob=None):
+def _fc_layer(input, fc_size, layer_name, keep_prob=None, l2scale=0.0):
     # get flat input size
     dim = np.prod(input.get_shape().as_list()[1:])
 
-    with tf.variable_scope(layer_name) as scope:
+    with tf.variable_scope(layer_name, regularizer=tf.contrib.layers.l2_regularizer(l2scale)) as scope:
         input_flat = tf.reshape(input, [-1, dim])
 
         weights = _weight_variable('weights', [dim, fc_size])
@@ -83,22 +82,22 @@ def _fc_layer(input, fc_size, layer_name, keep_prob=None):
     return output
 
 
-def inference(boxes, dataconfig, p_keep_conv, p_keep_hidden):
+def inference(boxes, dataconfig, p_keep_conv, p_keep_hidden, l2scale=0.0):
     prev_layer = boxes
 
-    prev_layer = _convlayer(prev_layer, 16, "conv1", keep_prob=p_keep_conv, pooling=True)
+    prev_layer = _convlayer(prev_layer, 16, "conv1", keep_prob=p_keep_conv, pooling=True, l2scale=l2scale)
 
-    prev_layer = _convlayer(prev_layer, 32, "conv2", keep_prob=p_keep_conv, pooling=True)
+    prev_layer = _convlayer(prev_layer, 32, "conv2", keep_prob=p_keep_conv, pooling=True, l2scale=l2scale)
 
-    with tf.variable_scope('conv3_multi') as scope:
-        prev_layer = _convlayer(prev_layer, 64, "conv3_1", keep_prob=None, pooling=False)
-        prev_layer = _convlayer(prev_layer, 64, "conv3_2", keep_prob=None, pooling=False)
-        prev_layer = _convlayer(prev_layer, 32, "conv3_3", keep_prob=p_keep_conv, pooling=True)
+    with tf.variable_scope('conv3_multi'):
+        prev_layer = _convlayer(prev_layer, 64, "conv3_1", keep_prob=None, pooling=False, l2scale=l2scale)
+        prev_layer = _convlayer(prev_layer, 64, "conv3_2", keep_prob=None, pooling=False, l2scale=l2scale)
+        prev_layer = _convlayer(prev_layer, 32, "conv3_3", keep_prob=p_keep_conv, pooling=True, l2scale=l2scale)
 
-    prev_layer = _fc_layer(prev_layer, 1024, "local4", keep_prob=p_keep_hidden)
-    prev_layer = _fc_layer(prev_layer, 1024, "local5", keep_prob=p_keep_hidden)
+    prev_layer = _fc_layer(prev_layer, 1024, "local4", keep_prob=p_keep_hidden, l2scale=l2scale)
+    prev_layer = _fc_layer(prev_layer, 1024, "local5", keep_prob=p_keep_hidden, l2scale=l2scale)
 
-    with tf.variable_scope('softmax_linear') as scope:
+    with tf.variable_scope('softmax_linear', regularizer=tf.contrib.layers.l2_regularizer(l2scale)) as scope:
         dim = np.prod(prev_layer.get_shape().as_list()[1:])
         weights = _weight_variable('weights', [dim, dataconfig.num_classes])
         biases = _bias_variable('biases', [dataconfig.num_classes])
@@ -109,11 +108,19 @@ def inference(boxes, dataconfig, p_keep_conv, p_keep_hidden):
 
 def loss(logits, labels):
 
-    with tf.variable_scope('loss') as scope:
+    with tf.variable_scope('loss'):
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits, labels, name='crossentropy')
 
         l = tf.reduce_mean(cross_entropy, name='crossentropy_mean')
+
+        # get regularization
+        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+        if len(reg_losses) != 0:
+            l2loss = tf.add_n(reg_losses, name="l2loss")
+            tf.scalar_summary(l2loss.name, l2loss)
+
+            l = tf.add(l, l2loss, name='total_loss')
 
     return l
 
@@ -135,7 +142,8 @@ def train(loss_op, learning_rate, learnrate_decay=0.95, global_step=None):
     tf.scalar_summary(loss_op.op.name, loss_op)
 
     # decay learning rate
-    learning_rate = tf.train.exponential_decay(learning_rate, global_step, 500, learnrate_decay, staircase=True, name="learning_rate")
+    learning_rate = tf.train.exponential_decay(learning_rate, global_step, 500, learnrate_decay,
+                                               staircase=True, name="learning_rate")
     tf.scalar_summary(learning_rate.op.name, learning_rate)
 
     # Create the gradient descent optimizer with the given learning rate.
