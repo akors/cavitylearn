06e9517b1be24234ad9450f38125eedcca7cbfdc
Alexander Korsunsky <a.korsunsky@gmail.com>
2016-06-13 13:22:38 +0200

    cavconvert: Move argument parser to the very top

--- src/py/cavconvert.py
+++ src/py/cavconvert.py
@@ -1,18 +1,104 @@
 #!/usr/bin/env python3
 
 import os
+import argparse
+import logging
+
+
+LOGDEFAULT = logging.INFO
+
+# ========================= Main argument parser ==========================
+parser_top = argparse.ArgumentParser(
+    description='Cavitylearn data management',
+    fromfile_prefix_chars='@')
+
+parser_top.add_argument('--loglevel', action="store",
+                        type=str.upper, dest='loglevel',
+                        metavar='LOGLEVEL',
+                        choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+                        default=LOGDEFAULT,
+                        help='Set log level to be LOG_LEVEL. Can be one of: DEBUG,INFO,WARNING,ERROR,CRITICAL')
+
+parser_top.add_argument('-j', '--jobs', action="store",
+                        type=int, dest='num_threads',
+                        metavar='NUM_THREADS',
+                        default=1,
+                        help='Use NUM_THREADS processors simultanously')
+
+subparsers = parser_top.add_subparsers(title='Actions', description='Data actions',
+                                       dest='main_action')
+
+# ========================= convertpcd argument parser ==========================
+parser_convertpcd = subparsers.add_parser('convertpcd',
+                                          help='Convert zip archives of PCD files into xz archives '
+                                               'with grids as numpy arrays.')
+
+parser_convertpcd.add_argument(action='store', nargs='+',
+                               type=str, dest='infiles',
+                               metavar="INPUT_FILE",
+                               help="List of cavity zip archives")
+
+parser_convertpcd.add_argument('--output_dir', '-o', action='store',
+                               type=str, dest='output_dir',
+                               metavar="OUTPUT_DIR",
+                               default=os.getcwd(),
+                               help="Output directory")
+
+parser_convertpcd.add_argument('--rotation_dir', action='store',
+                               type=str, dest='rotation_dir',
+                               metavar="ROTATION_DIR",
+                               help="Output directory for rotations")
+
+parser_convertpcd.add_argument('--resolution', action='store',
+                               type=float, dest='resolution',
+                               metavar="RES",
+                               default=0.375,
+                               help="Distance between the grid points in the same units as the PCD file")
+
+parser_convertpcd.add_argument('--shape', action='store', nargs=3,
+                               type=int, dest='shape',
+                               metavar="INT",
+                               required=True,
+                               help="3 Integegers with the size of the grid in X, Y and Z directions "
+                                    "separated by commas")
+
+parser_convertpcd.add_argument('--properties', action='store',
+                               type=str, dest='proplist',
+                               metavar="PROPERTY",
+                               required=True,
+                               help="List of properties separated by commas")
+
+parser_convertpcd.add_argument('--randrotations', action='store',
+                               type=int, dest='rotations',
+                               metavar="N",
+                               help="Create N random rotations of the cavities")
+
+
+# =========================     labelfile argument parser ==========================
+parser_labellist = subparsers.add_parser('labelfile',
+                                         help="Load labels for the cavities from the database, and store them as "
+                                              "list of UUID's with the labels in a tab-separated file")
+
+parser_labellist.add_argument(action='store', type=argparse.FileType('wt'), dest='outfile',
+                              metavar="OUTFILE",
+                              help="Output file for xz-compressed numpy label-array")
+
+parser_labellist.add_argument(action='store', nargs='+', type=str, dest='uuids', metavar="UUID",
+                              help="List of cavity UUID's")
+
+
+arguments = parser_top.parse_args()
+
+
 import sys
 # import lzma
 import concurrent.futures
 
-import logging
-import argparse
 
 from cavitylearn import converter
 
 # =============================== set up logging ==============================
 
-LOGDEFAULT = logging.INFO
 logger = logging.getLogger(__name__)
 
 
@@ -108,88 +194,6 @@ def main_labelfile(args, parser):
     for uuid, label in zip(args.uuids, labels):
         args.outfile.write("{uuid}\t{label}\n".format(uuid=uuid, label=label.decode("utf8")))
 
-
-# ========================= Main argument parser ==========================
-parser_top = argparse.ArgumentParser(
-    description='Cavitylearn data management',
-    fromfile_prefix_chars='@')
-
-parser_top.add_argument('--loglevel', action="store",
-                        type=str.upper, dest='loglevel',
-                        metavar='LOGLEVEL',
-                        choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
-                        default=LOGDEFAULT,
-                        help='Set log level to be LOG_LEVEL. Can be one of: DEBUG,INFO,WARNING,ERROR,CRITICAL')
-
-parser_top.add_argument('-j', '--jobs', action="store",
-                        type=int, dest='num_threads',
-                        metavar='NUM_THREADS',
-                        default=1,
-                        help='Use NUM_THREADS processors simultanously')
-
-subparsers = parser_top.add_subparsers(title='Actions', description='Data actions',
-                                       dest='main_action')
-
-# ========================= convertpcd argument parser ==========================
-parser_convertpcd = subparsers.add_parser('convertpcd',
-                                          help='Convert zip archives of PCD files into xz archives '
-                                               'with grids as numpy arrays.')
-
-parser_convertpcd.add_argument(action='store', nargs='+',
-                               type=str, dest='infiles',
-                               metavar="INPUT_FILE",
-                               help="List of cavity zip archives")
-
-parser_convertpcd.add_argument('--output_dir', '-o', action='store',
-                               type=str, dest='output_dir',
-                               metavar="OUTPUT_DIR",
-                               default=os.getcwd(),
-                               help="Output directory")
-
-parser_convertpcd.add_argument('--rotation_dir', action='store',
-                               type=str, dest='rotation_dir',
-                               metavar="ROTATION_DIR",
-                               help="Output directory for rotations")
-
-parser_convertpcd.add_argument('--resolution', action='store',
-                               type=float, dest='resolution',
-                               metavar="RES",
-                               default=0.375,
-                               help="Distance between the grid points in the same units as the PCD file")
-
-parser_convertpcd.add_argument('--shape', action='store', nargs=3,
-                               type=int, dest='shape',
-                               metavar="INT",
-                               required=True,
-                               help="3 Integegers with the size of the grid in X, Y and Z directions "
-                                    "separated by commas")
-
-parser_convertpcd.add_argument('--properties', action='store',
-                               type=str, dest='proplist',
-                               metavar="PROPERTY",
-                               required=True,
-                               help="List of properties separated by commas")
-
-parser_convertpcd.add_argument('--randrotations', action='store',
-                               type=int, dest='rotations',
-                               metavar="N",
-                               help="Create N random rotations of the cavities")
-
-
-# =========================     labelfile argument parser ==========================
-parser_labellist = subparsers.add_parser('labelfile',
-                                         help="Load labels for the cavities from the database, and store them as "
-                                              "list of UUID's with the labels in a tab-separated file")
-
-parser_labellist.add_argument(action='store', type=argparse.FileType('wt'), dest='outfile',
-                              metavar="OUTFILE",
-                              help="Output file for xz-compressed numpy label-array")
-
-parser_labellist.add_argument(action='store', nargs='+', type=str, dest='uuids', metavar="UUID",
-                              help="List of cavity UUID's")
-
-
-arguments = parser_top.parse_args()
 
 logging.basicConfig(level=arguments.loglevel, format='%(levelname)1s:%(message)s')
 
