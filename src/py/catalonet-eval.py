c06b99329f6dc305e97523d0e13ac31fa2fc94ea
Alexander Korsunsky <a.korsunsky@gmail.com>
2016-06-28 16:44:24 +0200

    catalonet-eval: add action "metric" to argument parser

--- src/py/catalonet-eval.py
+++ src/py/catalonet-eval.py
@@ -4,25 +4,29 @@
 import argparse
 import logging
 
-
 LOGDEFAULT = logging.INFO
 
-
 # ========================= Main argument parser ==========================
-parser_metrics = argparse.ArgumentParser(description='Catalophore neural network evaluation')
+parser_top = argparse.ArgumentParser(description='Catalophore neural network evaluation')
 
-parser_metrics.add_argument('--loglevel', action="store",
-                            type=str.upper, dest='log_level',
-                            metavar='LOG_LEVEL',
-                            choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
-                            default=LOGDEFAULT,
-                            help='Set log level to be LOG_LEVEL. Can be one of: DEBUG,INFO,WARNING,ERROR,CRITICAL')
+parser_top.add_argument('--loglevel', action="store",
+                        type=str.upper, dest='log_level',
+                        metavar='LOG_LEVEL',
+                        choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+                        default=LOGDEFAULT,
+                        help='Set log level to be LOG_LEVEL. Can be one of: DEBUG,INFO,WARNING,ERROR,CRITICAL')
 
-parser_metrics.add_argument('-j', '--jobs', action="store",
-                            type=int, dest='num_threads',
-                            metavar='NUM_THREADS',
-                            default=None,
-                            help='Use NUM_THREADS processors simultanously. Default is to use all processors.')
+parser_top.add_argument('-j', '--jobs', action="store",
+                        type=int, dest='num_threads',
+                        metavar='NUM_THREADS',
+                        default=None,
+                        help='Use NUM_THREADS processors simultanously. Default is to use all processors.')
+
+subparsers = parser_top.add_subparsers(title='Actions', description='Data actions',
+                                       dest='main_action')
+
+parser_metrics = subparsers.add_parser('metrics',
+                                       help="Calculate metrics for a trained neural net checkpoint once")
 
 parser_metrics.add_argument('--batchsize', action='store',
                             type=int, dest='batchsize',
@@ -34,32 +38,28 @@ parser_metrics.add_argument('--datasets', action='store',
                             type=str, dest='datasets',
                             metavar="DS",
                             help="List of datasets on which the net will be evaluated, separated by comma. If not "
-                             "specified, all datasets in DATADIR will be evaluated.")
+                                 "specified, all datasets in DATADIR will be evaluated.")
 
 parser_metrics.add_argument(action='store',
                             type=str, dest='dataset_dir',
                             metavar="DATADIR",
                             help="Dataset directory. This directory contains all the data and metadata files required "
-                             "for training.")
+                                 "for training.")
 
 parser_metrics.add_argument(action='store',
                             type=argparse.FileType("rb"), dest='checkpoint_file',
                             metavar="CHECKPOINT",
                             help="Path to the checkpoint file of the trained network.")
 
-args = parser_metrics.parse_args()
-
+args = parser_top.parse_args()
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=args.log_level, format='%(levelname)1s:%(message)s')
-
 
 import os
 import sys
 
 import numpy as np
-
-from cavitylearn import data
 
 import cavitylearn.evaluate
 import cavitylearn.data
@@ -115,7 +115,7 @@ def print_metrics(metrics, dataconfig):
         print("\n" + "#" * 80)
         print("Dataset", ds_name, ":")
 
-        print("\nAccuracy: %.2f %%" % (metric["accuracy"]*100.0))
+        print("\nAccuracy: %.2f %%" % (metric["accuracy"] * 100.0))
 
         print("\nConfusion Matrix:")
         prettyprint_confusion_metrics(metric["confusion_matrix"], dataconfig.classes)
@@ -133,28 +133,38 @@ def print_metrics(metrics, dataconfig):
         prettyprint_labeledarray(metric["g_score"], dataconfig.classes)
 
 
+def main_metrics(args, parser):
+    if pyprind:
+        progress_tracker = PyprindProgressTracker()
+    else:
+        progress_tracker = None
+
+    dataconfig = cavitylearn.data.read_dataconfig(os.path.join(args.dataset_dir, "datainfo.ini"))
+
+    if args.datasets is not None:
+        datasets = args.datasets.split(",")
+    else:
+        datasets = None
+
+    metrics = cavitylearn.evaluate.calc_metrics(dataset_dir=args.dataset_dir, checkpoint_path=args.checkpoint_file.name,
+                                                batchsize=args.batchsize,
+                                                dataset_names=datasets, num_threads=args.num_threads,
+                                                progress_tracker=progress_tracker)
+
+    print_metrics(metrics, dataconfig)
+
+    if progress_tracker:
+        progress_tracker.finish()
+
 # ========================= Script start ==========================
 
 
-if pyprind:
-    progress_tracker = PyprindProgressTracker()
+if not args.main_action:
+    parser_top.error('No action selected')
+elif args.main_action == 'metrics':
+    main_metrics(args, main_metrics)
 else:
-    progress_tracker = None
+    raise AssertionError("Unknown action {}".format(arguments.main_action))
 
 
-dataconfig = data.read_dataconfig(os.path.join(args.dataset_dir, "datainfo.ini"))
 
-if args.datasets is not None:
-    datasets = args.datasets.split(",")
-else:
-    datasets = None
-
-metrics = cavitylearn.evaluate.calc_metrics(dataset_dir=args.dataset_dir, checkpoint_path=args.checkpoint_file.name,
-                                            batchsize=args.batchsize,
-                                            dataset_names=datasets, num_threads=args.num_threads,
-                                            progress_tracker=progress_tracker)
-
-print_metrics(metrics, dataconfig)
-
-if progress_tracker:
-    progress_tracker.finish()
