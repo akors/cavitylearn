44f6704cdc97a921c8b29d6a8c7ab38714cbfb03
Alexander Korsunsky <a.korsunsky@gmail.com>
2016-07-18 14:07:30 +0200

    Add selectors for new testing datasets

---
+++ src/sql/cheaterset-cavities.sql
@@ -0,0 +1,9 @@
+SELECT uuid FROM fridge_cavities
+WHERE status = 3
+AND A_n != 0 AND C_n != 0 AND d_n != 0 AND e_n !=0
+AND shaped = 1
+AND cavitysearch_uuid IN ("568BC29C-9B76-4933-8448-6190A4E9E20D", "7B2361DA-D125-4E2E-877F-450804C5CFC6" )
+AND ligands IN ("SO4", "GOL", "EDO", "HEM", "NAG" )
+AND volume < 745 -- (0.375 * 30)^3 * 1/6 * pi
+AND volume > 20
+AND coverage_ligand_cavity > 50
