5c255ab067c19cac8b3134e714316975331ad2ce
Alexander Korsunsky <a.korsunsky@gmail.com>
2017-04-19 14:43:51 +0200

    catalonet0: Fix summary and variable names for visualization

--- src/py/cavitylearn/catalonet0.py
+++ src/py/cavitylearn/catalonet0.py
@@ -6,16 +6,14 @@ DTYPE = tf.float32
 
 
 def variable_summaries(var, name):
-    """Attach a lot of summaries to a Tensor."""
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean/' + name, mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-        tf.summary.scalar('sttdev/' + name, stddev)
-        tf.summary.scalar('max/' + name, tf.reduce_max(var))
-        tf.summary.scalar('min/' + name, tf.reduce_min(var))
-        #tf.histogram_summary(name, var)
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar(name + '/mean', mean)
+    with tf.name_scope('stddev'):
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar(name + '/stddev', stddev)
+    tf.summary.scalar(name + '/max', tf.reduce_max(var))
+    tf.summary.scalar(name + '/min', tf.reduce_min(var))
+    # tf.summary.histogram('histogram', var)
 
 
 def _weight_variable(name, shape, dtype=DTYPE):
@@ -31,16 +29,16 @@ def _convlayer(input, num_filters, layer_name, keep_prob=None, pooling=True, l2s
     with tf.variable_scope(layer_name, regularizer=tf.contrib.layers.l2_regularizer(l2scale)) as scope:
         kernel = _weight_variable('weights', [5, 5, 5, input.get_shape().as_list()[4], num_filters])
 
-        variable_summaries(kernel, layer_name + '/weights')
+        variable_summaries(kernel, name='weights')
 
         conv = tf.nn.conv3d(input, kernel, [1, 1, 1, 1, 1], padding='SAME')
         biases = _bias_variable('biases', [num_filters])
 
-        variable_summaries(biases, layer_name + '/biases')
+        variable_summaries(biases, name='biases')
 
         bias = tf.nn.bias_add(conv, biases)
 
-        # tf.histogram_summary(layer_name + '/pre_activations', bias)
+        # tf.histogram_summary('Pre-activations', bias)
 
         output = tf.nn.relu(bias, name=scope.name)
 
@@ -50,7 +48,7 @@ def _convlayer(input, num_filters, layer_name, keep_prob=None, pooling=True, l2s
         if keep_prob is not None:
             output = tf.nn.dropout(output, keep_prob)
 
-        # tf.histogram_summary(layer_name + '/activations', output)
+        # tf.histogram_summary('Activations', output)
 
     return output
 
@@ -63,21 +61,21 @@ def _fc_layer(input, fc_size, layer_name, keep_prob=None, l2scale=0.0):
         input_flat = tf.reshape(input, [-1, dim])
 
         weights = _weight_variable('weights', [dim, fc_size])
-        variable_summaries(weights, layer_name + '/weights')
+        variable_summaries(weights, name='weights')
 
         biases = _bias_variable('biases', [fc_size])
-        variable_summaries(biases, layer_name + '/biases')
+        variable_summaries(biases, name='biases')
 
         output = tf.matmul(input_flat, weights) + biases
 
-        # tf.histogram_summary(layer_name + '/pre_activations', output)
+        # tf.histogram_summary('pre_activations', output)
 
         output = tf.nn.relu(output, name=scope.name)
 
         if keep_prob is not None:
             output = tf.nn.dropout(output, keep_prob)
 
-        # tf.histogram_summary(layer_name + '/activations', output)
+        # tf.histogram_summary('activations', output)
 
     return output
 
@@ -107,21 +105,21 @@ def inference(boxes, dataconfig, p_keep_conv, p_keep_hidden, l2scale=0.0, l2scal
 
 
 def loss(logits, labels):
-
     with tf.variable_scope('loss'):
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-            logits=logits, labels=labels, name='crossentropy')
+            logits=logits, labels=labels, name='crossentropy_op')
 
-        l = tf.reduce_mean(cross_entropy, name='crossentropy_mean')
+        l = tf.reduce_mean(cross_entropy, name='crossentropy_mean_op')
 
         # get regularization
         reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
         if len(reg_losses) != 0:
-            l2loss = tf.add_n(reg_losses, name="l2loss")
-            tf.summary.scalar(l2loss.name, l2loss)
+            l2loss = tf.add_n(reg_losses, name="l2loss_op")
+            tf.summary.scalar("l2loss", l2loss)
 
-            l = tf.add(l, l2loss, name='total_loss')
+            l = tf.add(l, l2loss, name='total_loss_op')
 
+        tf.summary.scalar("loss", l)
     return l
 
 
@@ -138,13 +136,10 @@ def train(loss_op, learning_rate, learnrate_decay=0.95, learnrate_decay_freq=500
     train_op: The Op for training.
     """
 
-    # Add a scalar summary for the snapshot loss_op.
-    tf.summary.scalar(loss_op.op.name, loss_op)
-
     # decay learning rate
     learning_rate = tf.train.exponential_decay(learning_rate, global_step, learnrate_decay_freq, learnrate_decay,
-                                               staircase=True, name="learning_rate")
-    tf.summary.scalar(learning_rate.op.name, learning_rate)
+                                               staircase=True, name="learning_rate_op")
+    tf.summary.scalar("learning_rate", learning_rate)
 
     # Create the gradient descent optimizer with the given learning rate.
     # optimizer = tf.train.GradientDescentOptimizer(learning_rate)
@@ -161,13 +156,13 @@ def train(loss_op, learning_rate, learnrate_decay=0.95, learnrate_decay_freq=500
     return train_op
 
 
-def accuracy(logits, labels, k=1, name="accuracy"):
-
-    with tf.name_scope(name):
+def accuracy(logits, labels, k=1):
+    with tf.variable_scope("accuracy"):
         correct = tf.nn.in_top_k(logits, labels, k)
 
         # Return the number of true entries.
-        acc = tf.reduce_mean(tf.cast(correct, tf.float32), name=name)
-        tf.summary.scalar(name, acc)
+        acc = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy_op")
+
+        tf.summary.scalar("accuracy", acc)
 
     return acc
